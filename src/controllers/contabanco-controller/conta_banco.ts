4d54d90c7c1bae8c704770159899aefe6199399e
rzjprogramador <rzjprogramador@gmail.com>
2020-11-18 19:35:49 -0300

    Definidos Metodos Acessores get e set

--- src/controllers/contabanco-controller/conta_banco.ts
+++ src/controllers/contabanco-controller/conta_banco.ts
@@ -16,6 +16,40 @@ class ContaBanco {
     this.status = status
   }
 
+  set setNumConta(n: number) {
+    this.numConta = n
+  }
+  get getNumConta() {
+    return this.numConta
+  }
+
+  set setTipo(t: string) {
+    this.tipo = t
+  }
+  get getTipo() {
+    return this.tipo
+  }
+
+  set setDono(d: string) {
+    this.dono = d
+  }
+  get getDono() {
+    return this.dono
+  }
+
+  set setSaldo(s: number) {
+    this.saldo = s
+  }
+  get getSaldo() {
+    return this.saldo
+  }
+  set setStatus(sd: boolean) {
+    this.status = sd
+  }
+  get getStatus() {
+    return this.status
+  }
+
   public abrirConta(): void {}
   public fecharConta(): void {}
   public depositar(): void {}
